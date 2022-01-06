1d6276cb3f10f748fc49d93cfc43ca7b412cf07f
juuliana <jumaju10@hotmail.com>
2022-01-05 22:32:59 -0300

    feat: create context, api and function utils

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png"></link>
         </Head>
 
         <body>
